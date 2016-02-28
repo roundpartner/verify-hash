01f2ff811b7cef27b6aa2182700977e4661e9c08
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-02-28 18:10:21 +0000

    Create hash verification class with passing tests

--- src/VerifyHash/VerifyHash.php
+++ src/VerifyHash/VerifyHash.php
@@ -5,5 +5,56 @@ namespace VerifyHash;
 class VerifyHash
 {
 
+    const DEFAULT_ALGORITHM = 'sha1';
+
+    protected $secret;
+
+    public function __construct($secret)
+    {
+        $this->secret = $secret;
+    }
+
+    public function hash($content, $algorithm = self::DEFAULT_ALGORITHM)
+    {
+        if (!is_string($content)) {
+            return false;
+        }
+
+        if (!in_array($algorithm, hash_algos(), true)) {
+            return false;
+        }
+
+        return hash_hmac($algorithm, $content, $this->secret);
+    }
+
+    public function verify($hash, $content, $algorithm = self::DEFAULT_ALGORITHM)
+    {
+        return $this->hashEquals($hash, $this->hash($content, $algorithm));
+    }
+
+    /**
+     * String comparison for hashes
+     *
+     * @param $knownString
+     * @param $userString
+     * @return bool
+     */
+    private function hashEquals($knownString, $userString)
+    {
+        if (function_exists('hash_equals')) {
+            return hash_equals($knownString, $userString);
+        }
+
+        if (strlen($knownString) !== strlen($userString)) {
+            return false;
+        }
+
+        $res = $knownString ^ $userString;
+        $ret = 0;
+        for ($i = strlen($res) - 1; $i >= 0; $i--) {
+            $ret |= ord($res[$i]);
+        }
+        return !$ret;
+    }
 
 }
---
+++ tests/Unit/VerifyHashTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class VerifyHashTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testHash()
+    {
+        $verifyHash = new \VerifyHash\VerifyHash('reallysecurestring');
+        $this->assertEquals('dc91e632a81bba220a002d46e06da80dc2753591', $verifyHash->hash('This is a string I want to hash'));
+    }
+
+    public function testVerify()
+    {
+        $verifyHash = new \VerifyHash\VerifyHash('reallysecurestring');
+        $this->assertTrue($verifyHash->verify('dc91e632a81bba220a002d46e06da80dc2753591', 'This is a string I want to hash'));
+    }
+
+}
